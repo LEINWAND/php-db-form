9b5d7db16d081460e4ec551f74679fdb21198cb8
Iosif Miclaus <iosif.miclaus@gmail.com>
2017-01-10 00:17:45 +0100

    Further improvements

--- action.php
+++ action.php
@@ -1,13 +1,18 @@
 <?php
 
-$post_action = isset($_POST['action']) && ! empty($_POST['action'] ? $_POST['action'] : null;
+$post_action = isset($_POST['action']) && ! empty($_POST['action']) ? $_POST['action'] : null;
 
 if ( ! is_null($post_action)) {
   switch ($post_action) {
-    case 'save_email': 
-      save_email(); 
+    case 'add_email':
+      $address = isset($_POST['email']) && ! empty($_POST['email']) ? $_POST['email'] : null;
+      if ( ! is_null($address)) {
+        add_email($address);
+      } else {
+        show_index('invalid email address !');
+      }
       break;
-    default: 
+    default:
       show_index();
       break;
   }
@@ -18,14 +23,27 @@ if ( ! is_null($post_action)) {
 
 /// functions
 
-function save_email() {
-  // connect to database
-  
+function add_email() {
+  // connect to database (NOTE: configure as needed)
+  $host = 'localhost';
+  $db = 'php-db-form';
+  $user = 'root';
+  $pass = 'root';
+
+  $pdo = new PDO("pgsql:host=$host;port=8889;dbname=$db;user=$user;password=$pass");
+
+  $stmt = $pdo->prepare('INSERT INTO emails (address, created_at) VALUES (:address, :created_at)');
+  // $stmt->bindParam(':address', );
+
   // create prepared statement, save to db
-  
-  show_index():
+
+  show_index();
 }
 
-function show_index() {
-  header('Location: index.php');
+function show_index($msg = null) {
+  if ( is_null($msg)) {
+    header('Location: index.php');
+  } else {
+    header('Location: index.php?error' . $msg);
+  }
 }
--- index.php
+++ index.php
@@ -6,34 +6,64 @@
 
 $data = ['some@email.com', 'office@email.org'];
 
+$error = isset($_GET['error']) && ! empty($_GET['error']) ? $_GET['error'] : null;
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8" />
   <title>Database Form</title>
+
+  <style>
+    .DBForm {
+      border: 2px solid #222;
+      border-radius: 2px;
+      padding: 1rem;
+    }
+    .DBForm .input {
+      width: 222px;
+    }
+    .DBForm .error {
+      color: tomato;
+      padding-top: 0.4rem;
+    }
+
+    .Fields .field {
+      border-bottom: 1px solid #222;
+      padding: 1rem;
+    }
+  </style>
 </head>
 <body>
   <h1>Database Form</h1>
-  <form method="post" action="action.php">
-    <input 
-      type="email" 
-      name="email" 
+  <form
+    method="post"
+    action="action.php"
+    class="DBForm"
+  >
+    <input
+      type="email"
+      name="email"
       placeholder="Type your email address"
+      class="input"
     />
-    <input 
+    <input
       type="submit"
-      name="save_email"
-      value="Save"
+      name="add_email"
+      value="add"
      />
+     <div class="error">
+       <?php echo $error; ?>
+     </div>
   </form>
-  
+
   <h2>Email addresses</h2>
-  <div>
+  <div class="Fields">
     <?php
       foreach ($data as $key => $email) {
-        ?><div class="field"><?php 
-          echo $email; 
+        ?><div class="field"><?php
+          echo $key . ' – ' . $email;
         ?></div><?php
       }
     ?>
